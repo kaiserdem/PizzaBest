25f36c4739fb94aa8dd8b3bc3f9246a49da04fd9
kaiserdem <>
2022-09-20 17:57:56 +0300

    implementation ui of authorization

--- PizzaBest/ContentView.swift
+++ PizzaBest/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Authorization")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("whiteAplha"))
+                .cornerRadius(35)
+            
+            VStack {
+                TextField("Input text", text: $email)
+                    .padding()
+                    .background(Color("whiteAplha"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                SecureField("Input password", text: $password)
+                    .padding()
+                    .background(Color("whiteAplha"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                Button {
+                    print("auth")
+                } label: {
+                    Text("Enter")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [Color("yellow"), Color("orange")], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("darkBrown"))
+                        
+                }
+                
+                Button {
+                    print("Not with us yet")
+                } label: {
+                    Text("Not with us yet?")
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("darkBrown"))
+                        
+                }
+            }
             .padding()
+            .padding(.top, 16)
+            .background(Color("whiteAplha"))
+            .cornerRadius(24)
+            .padding(30)
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+         .background(Image("background").ignoresSafeArea())
     }
 }
 
