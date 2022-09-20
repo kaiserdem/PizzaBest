9c45127dc2715c11aa546db6ea4d41ddcad90d47
kaiserdem <>
2022-09-20 16:31:14 +0300

    first commit

---
+++ PizzaBest/PizzaBestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaBestApp.swift
+//  PizzaBest
+//
+//  Created by kaiserdem  on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
